858435041c8e39241b35d54040a67cd860956d9f
hucheng <hucheng@qq.com>
2019-01-05 00:31:15 +0800

    Eclipse

---
+++ src/main/java/com/hucheng/svn/entity/Employee.java
@@ -0,0 +1,11 @@
+package com.hucheng.svn.entity;
+
+public class Employee {
+	public static void main(String[] args) {
+		System.out.println("2019年1月4日19:48:32");
+
+		System.out.println("2019年1月4日19:53:31");
+
+
+	}
+}
---
+++ src/test/java/com/hucheng/svn/test/SVNTest.java
@@ -0,0 +1,5 @@
+package com.hucheng.svn.test;
+
+public class SVNTest {
+
+}
